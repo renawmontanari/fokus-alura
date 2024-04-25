0b4b6330caa9a70800818e1d30dee5f4ee2ef503
renawmontanari <regiangelica@gmail.com>
2024-04-25 20:25:11 -0300

    add-musica

--- script.js
+++ script.js
@@ -5,20 +5,38 @@ const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
 
 focoBtn.addEventListener('click', () => {
     alterarContexto('foco');
+    focoBtn.classList.add('active');
 });
 
 curtoBtn.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    curtoBtn.classList.add('active');
 });
 
 longoBtn.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    longoBtn.classList.add('active');
 });
 
 function alterarContexto(contexto) {
+
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active');
+    });
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     switch (contexto) {
@@ -39,4 +57,5 @@ function alterarContexto(contexto) {
         default:
             break;
     } 
+
 };
